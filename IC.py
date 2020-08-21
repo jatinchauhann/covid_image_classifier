a04ed9f8de41fb3fd6dc922458f0b96edd0abf8e
jatinchauhann <jtnchauhan24@gmail.com>
2020-08-22 03:16:42 +0530

    added js blob FIle

--- IC.py
+++ IC.py
@@ -105,10 +105,10 @@ def function_pred_grad_cam(IMAGE_PATH):
 
     plt.imshow(img_)
     # plt.show()
-    plt.savefig('input.png')
+    plt.savefig('uploads/input.png')
     # Show the image with matplotlib
     plt.imshow(image)
     # plt.show()
-    plt.savefig('output.png')
+    plt.savefig('uploads/output.png')
 
     return "DONE"
